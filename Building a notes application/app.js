5d5771ebc1a2241ee8cdc138e7ab0532fab09953
AliMahmoud26 <alimah.engineer@gmail.com>
2026-06-14 22:18:10 +0300

    Finishing selecting html elements needed for the notes app functionality

---
+++ Building a notes application/app.js
@@ -0,0 +1,10 @@
+const addBox = document.querySelector(".add-box"),
+  popupBox = document.querySelector(".popup-box"),
+  closeIcon = popupBox.querySelector("header i"),
+  titleTag = popupBox.querySelector("input"),
+  descTag = popupBox.querySelector("textarea"),
+  addBtn = popupBox.querySelector("button");
+
+// Array of months
+const months = ["January", "February", "March", "April", "May", "June", "July",
+  "August", "September", "October", "November", "December"];
